7d43e949f2f0d6aee0b4865632bb050a41416123
Loi Vo Tan <99393329+dlowji@users.noreply.github.com>
2023-06-26 13:47:42 +0700

    Update metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,13 +36,10 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 ogp_custom_meta_tags = [
-    '<meta name="keywords" content="Dương Hữu Phúc, Duong Huu Phuc, Phuc H. Duong, Ton Duc Thang University" />',
-    '<meta property="og:title" content="Homepage | Dương Hữu Phúc [Phuc H. Duong]" />',
-    '<meta property="og:type" content="website" />',
-    '<meta property="og:url" content="https://www.duonghuuphuc.com/" />',
-    '<meta property="og:locale" content="en_VN" />',
-    '<meta property="og:logo" content="https://www.duonghuuphuc.com/favicon.png" size="256x25" />',
+    '<meta name="keywords" content="Dương Hữu Phúc, Duong Huu Phuc, Phuc H. Duong, Ton Duc Thang University, Open edX, Tutor" />',
+    '<meta property="og:type" content="article" />',
+    '<meta property="og:url" content="https://edx-docs.fastai.dev/vi/latest/" />',
+    '<meta property="og:locale" content="vi" />',
     '<meta property="og:image" content="https://fastai.dev/static/img/fb-og-edx-docs-20230623.png" />',
-    '<meta property="og:description" content="Phuc Duong is currently a lecturer and researcher at the Faculty of IT, TDTU (Vietnam), and at the R&D of NewAI Vietnam to research and develop AI solutions." />',
-    '<meta property="fb:app_id" content="858663771713993" />'
+    '<meta property="og:description" content="This is series of articles that utilize Open edX as the primary platform to illustrate digital learning activities such as: present the installation and deployment process of the Open edX platform, explore effective utilization strategies for the Open edX platform. Each article will cater to specific reader profiles, such as programmers, educators, learners, and education administrators, to ensure easy access to the content." />',
 ]
