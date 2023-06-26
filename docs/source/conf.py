edd7bb69ad9288d6c2415f8cdc8ff4a382f1e78e
Loi Vo Tan <99393329+dlowji@users.noreply.github.com>
2023-06-26 15:09:48 +0700

    Update metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,11 +35,17 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
+
+# -- Options for Open Graph
+
+ogp_site_url = "https://edx-docs.fastai.dev/vi/latest/"
+ogp_image = "https://fastai.dev/static/img/fb-og-edx-docs-20230623.png"
+ogp_description_length = 300
+ogp_type = "article"
+ogp_enable_meta_description = True
+
 ogp_custom_meta_tags = [
     '<meta name="keywords" content="Dương Hữu Phúc, Duong Huu Phuc, Phuc H. Duong, Ton Duc Thang University, Open edX, Tutor" />',
-    '<meta property="og:type" content="article" />',
-    '<meta property="og:url" content="https://edx-docs.fastai.dev/vi/latest/" />',
     '<meta property="og:locale" content="vi" />',
-    '<meta property="og:image" content="https://fastai.dev/static/img/fb-og-edx-docs-20230623.png" />',
     '<meta property="og:description" content="This is series of articles that utilize Open edX as the primary platform to illustrate digital learning activities. Each article will cater to specific reader profiles, such as programmers, educators, learners, and education administrators, to ensure easy access to the content." />',
 ]
